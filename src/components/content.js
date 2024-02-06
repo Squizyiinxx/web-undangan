f6b463057b8f25458ef447bbd5192bf8ee3d2bc7
Squizyiinxx <wowmr51@gmail.com>
2024-02-06 13:15:48 +0700

    first

---
+++ src/components/content.js
@@ -0,0 +1,40 @@
+const navList = [
+  {
+    link: "#home",
+    icon: "mdi:home",
+  },
+  {
+    link: "#profile",
+    icon: "mdi:person",
+  },
+  {
+    link: "#acara",
+    icon: "mdi:calendar",
+  },
+  {
+    name: "lokasi",
+    link: "#lokasi",
+    icon: "mdi:location",
+  },
+  {
+    name: "gallery",
+    link: "#gallery",
+    icon: "mdi:insert-photo",
+  },
+  {
+    name: "cerita",
+    link: "#cerita",
+    icon: "mdi:pencil",
+  },
+  {
+    name: "gift",
+    link: "#gift",
+    icon: "mdi:gift",
+  },
+  {
+    name: "ucapan",
+    link: "#ucapan",
+    icon: "mdi:message",
+  },
+];
+export { navList };
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Hero from "./hero.astro";
+import Profile from "./profile.astro";
+import UndanganAcara from "./undanganAcara.astro";
+import Ucapan from "./ucapan.astro";
+import Story from "./story.astro";
+import Location from "./location.astro";
+import Gallery from "./gallery.astro";
+import Navigation from "./Navigation.astro";
+import Video from "./video.astro";
+import Gift from "./gift.astro";
+import Footer from "./footer.astro";
+
+export {
+    Hero,
+    Profile,
+    UndanganAcara,
+    Ucapan,
+    Story,
+    Location,
+    Gallery,
+    Navigation,
+    Video,
+    Gift,
+    Footer,
+}
