e52a203e6be39a5c37d32a0e0e21e5458d2861b5
Squizyiinxx <99987609+Squizyiinxx@users.noreply.github.com>
2024-07-19 11:35:26 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import {astroImageTools} from "astro-imagetools";
-import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-   adapter: vercel(),
   integrations: [tailwind(), icon(), astroImageTools],
 });
